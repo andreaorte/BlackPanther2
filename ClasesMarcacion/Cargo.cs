11feed6158023daadc55516e4a96925f0d8ae8df
StevenCardozo <steven.cardozoff@gmail.com>
2019-09-14 10:15:48 -0400

    Creacion de la Biblioteca de Clase: ClasesMarcacion y la Clase Cargo.

---
+++ ClasesMarcacion/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesMarcacion
+{
+    public class Cargo
+    {
+        private int idCargo { get; set; }
+        private String descripcion { get; set; }
+
+        public Cargo() {  }
+        public Cargo(int idCargo, String decripcion)
+        {
+            this.idCargo = idCargo;
+            this.descripcion = descripcion;
+
+        }
+    }
+}
