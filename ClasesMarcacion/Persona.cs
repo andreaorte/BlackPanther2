37f5a5c8d96f49d8ad440071516de68780a8c715
andreaorte <andreaorte97@gmail.com>
2019-09-15 17:01:50 -0400

    CreacionClasePersona

---
+++ ClasesMarcacion/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesMarcacion
+{
+    public class Persona
+    {
+        public int CodigoHumano { get; set; }
+        public string NroDocumento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Persona() { }
+
+        public Persona(int codigoHumano, string nroDocumento, string nombre, string apellido)
+        {
+            this.CodigoHumano = codigoHumano;
+            this.NroDocumento = nroDocumento;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+
+    }
+}
