8313d701ff09460eb6ae38c95f893adbeb1f5f90
DropeArias <dropearias@gmail.com>
2019-09-15 12:04:08 -0400

    Creacion de clase TipoHora

---
+++ ClasesMarcacion/TipoHora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesMarcacion
+{
+    class TipoHora
+    {
+        private int Id { get; set; }
+        private String descripcion { get; set; }
+
+        public TipoHora() {  }
+
+        public TipoHora(int Id, string descripcion )
+        {
+
+            this.Id = Id;
+            this.descripcion = descripcion;
+
+        }
+    }
+}
