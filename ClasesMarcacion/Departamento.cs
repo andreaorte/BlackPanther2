f63961d6f0073cd6eb193fadfb79cb5d2653d98c
jose escurra <jocscurra_1@hotmail.com>
2019-09-16 18:39:33 -0400

    Creacion de clase Departamento

---
+++ ClasesMarcacion/Departamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesMarcacion
+{
+    class Departamento
+    {
+        public int Id { get; set; }
+        public String descripcion { get; set; }
+
+        public Departamento() { }
+        public Departamento(int Id, string descripcion)
+        {
+
+            this.Id = Id;
+            this.descripcion = descripcion;
+
+        }
+                     
+    }
+}
